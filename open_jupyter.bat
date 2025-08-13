2f261db7fbae4756514f230685ec44f2dd3f235b
Shamsul Arifin <arifinbd6@gmail.com>
2025-08-13 16:02:57 +0600

    changes to the script

--- open_jupyter.bat
+++ open_jupyter.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Change this to the folder you want to open
-set "target_dir=C:\path\to\your\notebook\folder"
+set "target_dir=D:\Arifin Backup\Projects\Py_DS_ML"
 
 REM Open Windows Terminal with Command Prompt profile
 start wt -p "Command Prompt" cmd /k "cd /d %target_dir% && jupyter notebook"
